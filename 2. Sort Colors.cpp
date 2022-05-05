c5a51f9d025b8028feb8855e4307fd93c0318063
GUNJAN GUPTA <81305824+gunjan-g@users.noreply.github.com>
2022-05-05 22:25:03 +0530

    Problem 2

---
+++ 2. Sort Colors.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    void sortColors(vector<int>& nums) {
+        if(nums.size()== 0) return;
+        int start=0, mid=0, end=nums.size()-1;
+               
+        while(mid<=end){
+            if(nums[mid]==0){
+                swap(nums[mid], nums[start]);
+                mid++;
+                start++;
+            }         
+            else if(nums[mid]==1) mid++;
+            else if(nums[mid]==2){
+                swap(nums[mid], nums[end]);
+                end--;
+            }
+        }
+    }
+};
