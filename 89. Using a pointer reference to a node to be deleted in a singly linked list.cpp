a04bff027dc51306d8ad59bd06e1e9100ce86765
GUNJAN GUPTA <81305824+gunjan-g@users.noreply.github.com>
2022-05-25 22:14:39 +0530

    Problem 89

---
+++ 89. Using a pointer reference to a node to be deleted in a singly linked list.cpp
@@ -0,0 +1,14 @@
+void deleteNode(Node* node)
+{
+	Node* prev;
+	if (node == NULL)
+		return;
+	else {
+		while (node->next != NULL) {
+			node->data = node->next->data;
+			prev = node;
+			node = node->next;
+		}
+		prev->next = NULL;
+	}
+}
