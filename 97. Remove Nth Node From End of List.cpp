3decb5fae7965be1824246917f08ae702f83360e
GUNJAN GUPTA <81305824+gunjan-g@users.noreply.github.com>
2022-05-26 22:52:56 +0530

    Problem 97

---
+++ 97. Remove Nth Node From End of List.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    ListNode* removeNthFromEnd(ListNode* head, int n) {
+        ListNode *fast=head, *slow=head;
+        for(int i=0;i<n;i++) fast=fast->next;
+        if(fast==NULL) return head->next;
+        while(fast->next!=NULL){
+            fast=fast->next;
+            slow=slow->next;
+        }
+        slow->next=slow->next->next;
+        return head;
+    }
+};
